bb5d762abd7ad3b50e0344c3bf8dfe2834a767a2
aastharasekar <aastharasekar3@gmail.com>
2025-07-28 01:51:09 +0530

    fix extra column

--- Q5.java
+++ Q5.java
@@ -6,11 +6,9 @@ class Q5 {
         int rows = sc.nextInt();
         int cols = rows;
 
-        int num = 1;
         for(int i = 1; i <= rows; i++) {
             for(int j = 1; j <= cols; j++) {
-                System.out.print(num + " ");
-                    
+                System.out.print(i + " ");
             }
             System.out.println();
         }
